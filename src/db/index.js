a60bce96fe0ef02b86efbe13834c397a7edd1833
vanshparmar1 <iamvanshparmar@gmail.com>
2025-10-02 14:24:24 +0530

    connect to mongo atlas

---
+++ src/db/index.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+
+
+const connectDB =async()=>{
+    try {
+       const ConnectionInstance=  await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+       console.log(`Connected to DB !!!! DB host:${ConnectionInstance.connection.host}`)
+        
+    } catch (error) {
+        console.error("Errrr:", error)
+        process.exit(1)
+        
+    }
+} 
+export default connectDB
---
+++ src/index.js
@@ -0,0 +1,31 @@
+import dotenv from "dotenv"
+dotenv.config({
+    path:"./env"
+})
+import connectDB from "./db/index.js";
+
+ connectDB()
+
+
+
+
+
+
+
+
+
+// const app=express()
+// (async()=>{
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error", (error)=>{
+//             console.error("ERROR",error);
+//             throw error
+//         })
+//         app.listen(process.env.PORT, ()=>{
+//             console.log(`Server is running on port ${process.env.PORT}`);})
+//     } catch (error) {
+//         console.error("Errrr:", error)
+//         throw error
+//     }
+// })()  
