637ffe880db2a66b8f3fd27098c550d9f804e691
vanshparmar1 <iamvanshparmar@gmail.com>
2025-12-22 13:24:41 +0530

    add register controller

--- src/app.js
+++ src/app.js
@@ -1,16 +1,27 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import CORS_ORIGIN from "../.env"
- 
-const app = express()
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    'credentials': true
-}))
-app.use(express.json({limit: '16kb'}))
-app.use(express.urlencoded({extended: true}))
-app.use(cookieParser())
-app.use(express.static('public'))
 
-export default app;
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+//routes import
+import userRouter from "./routes/user.routes.js";
+
+//routes declaration
+app.use("/api/v1/users", userRouter);
+
+// http://localhost:8000/api/v1/users/register
+
+export { app };
